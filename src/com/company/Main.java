053c9c5e0c6cf0c49784d4f0d1ea23219b763193
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-30 14:39:30 -0500

    ZorkProject corrected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,25 @@
 package com.company;
 
 import java.awt.event.KeyEvent;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
+    public static void inputCheck(Scanner sc) {
+        try {
+            System.out.println("Please enter a room number: ");
+            int newInput = sc.nextInt();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("That is not an integer, please try again.");
+        }
+
+    }
+
     public static void main(String[] args) {
-    Room startGame = new Room();
+        Room startGame = new Room();
+        Scanner scan=new  Scanner(System.in);
+
     }
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,6 +11,7 @@ public class Room {
     int input;
     static boolean start = true;
     static Scanner scan = new Scanner(System.in);
+
     Random random = new Random();
     HashMap<Integer, String> items = new HashMap<Integer, String>();
     HashMap<Integer, String> roomName = new HashMap<Integer, String>();
@@ -26,37 +28,58 @@ public class Room {
         }
     }
 
+    public void checkInput () {
+        boolean correctInput = false;
+//        System.out.println("Please enter a room number: ");
+        while (correctInput == false) {
+        try {
+            correctInput = true;
+            input = scan.nextInt();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("That is not an integer, please try again.");
+            correctInput = false; // if the programs steps into the catch block here, it must mean they gave me a wrong input
+            scan.nextLine(); // this is to clear scanner buffer
+            continue; // go to next iteration of loop
+        }
+    }
+  }
 
     public void room1() {
         this.roomName.put(1, "Foyer");
         this.items.put(1, "Dead Scorpion");
-        System.out.println("You have entered room: " + roomName.get(1));
-        System.out.println("The item is : " + items.get(1));
         while (start) {
-            System.out.println("Enter what room you want to enter:\n1. Front Room\n0. Exit");
-            input = scan.nextInt();
+
+            System.out.println("Please enter a room number: ");
+            System.out.println(" \n1. Front Room\n0. Exit");
+            checkInput();
+
+           //input = scan.nextInt();
             if (input == 1) {
                 room2();
+
             } else if (input == 0) {
                 exit();
                 start = false;
+
             } else {
-                System.out.println("Enter valid input");
-                continue;
+              continue;
             }
         }
+        System.out.println("You have entered room: " + roomName.get(1));
+        System.out.println("The item is : " + items.get(1));
     }
 
     public void room2() {
 
         this.roomName.put(2, "Front Room");
         this.items.put(2, "Dead Scorpion");
-        System.out.println("You have entered room: " + roomName.get(2));
-        System.out.println("The item is : " + items.get(2));
         while (start) {
-            System.out.println("Enter what room you want to enter:" +
+
+            System.out.println("You have enter the following rooms: " +
                     "\n1. Go back \n2. Library \n3. Kitchen");
-            input = scan.nextInt();
+            checkInput();
+//           input = scan.nextInt();
             if (input == 1) {
                 System.out.println("Going back");
                 room1();
@@ -71,19 +94,19 @@ public class Room {
                 continue;
             }
         }
+        System.out.println("You have entered room: " + roomName.get(2));
+        System.out.println("The item is : " + items.get(2));
     }
 
 
     public void room3() {
         this.roomName.put(3, "Library");
         this.items.put(3, "Spiders");
-        System.out.println("You have entered the " + roomName.get(3));
-        System.out.println("The item is : " + items.get(3));
-
         while (start) {
-            System.out.println("Enter what room you want to enter:" +
-                    "\n1. Front Room\n2.Dining Room");
-            int input = scan.nextInt();
+            System.out.println("You have enter the following rooms:" +
+                    "\n1. Front Room\n2. Dining Room");
+            checkInput();
+//          int input = scan.nextInt();
             if (input == 1) {
                 roomNumChoice = input;
                 room2();
@@ -98,19 +121,17 @@ public class Room {
                 continue;
             }
         }
+        System.out.println("You have entered the " + roomName.get(3));
+        System.out.println("The item is : " + items.get(3));
     }
 
     public void room4() {
         this.roomName.put(4, "Kitchen.");
         this.items.put(4, "Bats");
-
-
-        System.out.println("You have entered the: " + roomName.get(4));
-        System.out.println("The item is : " + items.get(4));
-
         while (start) {
-            System.out.println("Enter what room you want to enter: \n1.Front Room\n2. Parlor");
-            int input = scan.nextInt();
+            System.out.println("You have enter the following rooms: \n1.Front Room\n2. Parlor");
+            checkInput();
+//          int input = scan.nextInt();
             if (input == 1) {
                 System.out.println("You are going to the Front Room");
                 room2();
@@ -125,19 +146,17 @@ public class Room {
                 continue;
             }
         }
+        System.out.println("You have entered the: " + roomName.get(4));
+        System.out.println("The item is : " + items.get(4));
     }
 
     public void room5() {
         this.roomName.put(5, "Dining room");
         this.items.put(5, "Dust Empty Box");
-
-
-        System.out.println("You have entered the: " + roomName.get(5));
-        System.out.println("The item is : " + items.get(5));
-
         while (start) {
-            System.out.println("Enter what room you want to enter: \n1. Library");
-            int input = scan.nextInt();
+            System.out.println("You have enter the following room: \n1. Library");
+            checkInput();
+//          int input = scan.nextInt();
             if (input == 1) {
                 System.out.println("You are going to the Library");
                 room2();
@@ -149,19 +168,18 @@ public class Room {
                 continue;
             }
         }
+        System.out.println("You have entered the: " + roomName.get(5));
+        System.out.println("The item is : " + items.get(5));
     }
 
     public void room6() {
         this.roomName.put(6, "Vault");
         this.items.put(6, "3 Walking Skeletons");
         Random random = new Random();
-
-        System.out.println("You have entered the: " + roomName.get(6));
-        System.out.println("The item is : " + items.get(6));
-
         while (start) {
-            System.out.println("Enter what room you want to enter: \n1. Parlor\n2. Secret Room");
-            int input = scan.nextInt();
+            System.out.println("You have enter the following room: \n1. Parlor\n2. Secret Room");
+            checkInput();
+//          int input = scan.nextInt();
             if (input == 1) {
                 System.out.println("You are going to the Parlor");
                 room2();
@@ -176,16 +194,17 @@ public class Room {
                 continue;
             }
         }
-        }
-        public void room7(){
+        System.out.println("You have entered the: " + roomName.get(6));
+        System.out.println("The item is : " + items.get(6));
+    }
+
+    public void room7(){
             this.roomName.put(7, "Parlor");
             this.items.put(7, "treasure chest");
-            System.out.println("You have entered room: " + roomName.get(7));
-            System.out.println("The item is : " + items.get(7));
-
             while (start) {
-                System.out.println("Enter what room you want to enter:1. West room 6 | 2. South Room 4 ");
-                input = scan.nextInt();
+               System.out.println("You have enter the following room : \n1. West room 6 \n2. South Room 4 ");
+               checkInput();
+//             input = scan.nextInt();
                 if (input == 1) {
                     System.out.println("You are going to the west room");
                     room6();
@@ -200,6 +219,8 @@ public class Room {
                     continue;
                 }
             }
+            System.out.println("You have entered room: " + roomName.get(7));
+            System.out.println("The item is : " + items.get(7));
         }
 
         // Room 8 added here?
@@ -207,13 +228,9 @@ public class Room {
         public void room8(){
             this.roomName.put(8, "Secret Room");
             this.items.put(8, "Piles of gold");
-
-            System.out.println("You have entered room: " + roomName.get(8));
-            System.out.println("The item is : " + items.get(8));
-
             while (start) {
-                System.out.println("Enter what room you want to enter:1. West room 6");
-                int input = scan.nextInt();
+                System.out.println("You have enter the following room:\n1. West room 6");
+//              int input = scan.nextInt();
                 if (input == 1) {
                     roomNumChoice = input;
                     room6();
@@ -225,8 +242,8 @@ public class Room {
                     continue;
                 }
             }
+            System.out.println("You have entered room: " + roomName.get(8));
+            System.out.println("The item is : " + items.get(8));
         }
-
-
 }
 
